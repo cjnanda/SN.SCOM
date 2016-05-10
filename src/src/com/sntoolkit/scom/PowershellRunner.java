4f1b471901ab928c8b2ae4256d5af03c7d72b3be
Chris Nanda <chris.nanda@servicenow.com>
2016-05-10 13:25:31 -0700

    Updated PSRunner

--- src/src/com/sntoolkit/scom/PowershellRunner.java
+++ src/src/com/sntoolkit/scom/PowershellRunner.java
@@ -1,42 +1,25 @@
 package com.sntoolkit.scom;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+
 import com.glide.util.ParamString;
+import com.service_now.mid.probe.Powershell;
 
 public class PowershellRunner {
 	 protected ParamString fCommandParam;
 	 protected ParamString fScriptParam;
+	 Map<String,String> fParameters;
+	 public PowershellRunner(Map<String,String> parameters){
+		 fParameters = parameters;
+	 }
+	 
+	 public void runPowershell(){
+			Powershell ps = new Powershell(fParameters);
+		}
 	 
 	 
-//	 protected String getCommand(File file, List<String> envVars)
-//	  {
-//	    return getCommand(file, "", "", envVars);
-//	  }
-//	  
-//	  protected String getCommand(File file, String user, String password, List<String> envVars)
-//	  {
-//	    this.fCommandParam.setParameter("powershell", getPowershellExe());
-//	    this.fCommandParam.setParameter("psscript", wrapParam(PSSCRIPTFILE));
-//	    this.fCommandParam.setParameter("computer", getComputer());
-//	    if (this.fCommandParameterPassing)
-//	    {
-//	      this.fCommandParam.setParameter("user", getUser(user));
-//	      this.fCommandParam.setParameter("password", getPassword(password));
-//	    }
-//	    this.fCommandParam.setParameter("usecred", StringUtil.notNil(user) ? USE_CREDENTIALS : NO_CREDENTIALS);
-//	    this.fCommandParam.setParameter("debug", isDebugging() ? DEBUG_TRUE : DEBUG_FALSE);
-//	    
-//	    this.fScriptParam.setParameter("script", wrapParam(file.getAbsolutePath()));
-//	    this.fCommandParam.setParameter("userscript", this.fScriptParam.toString());
-//	    
-//	    this.fCommandParam.setParameter("isdisco", hasParameter("workflow") ? IS_RUNBOOK : IS_DISCOVERY);
-//	    
-//	    List list = new ArrayList();
-//	    if (addScriptParameters(list, envVars) != 0) {
-//	      return null;
-//	    }
-//	    this.fCommandParam.setParameter("genericparams", list.size() > 0 ? StringUtil.join(list, " ") : "");
-//	    
-//	    return this.fCommandParam.toString();
-//	  }
+	 
 	  
 }
