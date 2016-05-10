ca201ca5a871c5bcf59e9e84bd8cb3cd4d5ec665
Chris Nanda <cjnanda@gmail.com>
2016-05-10 20:26:33 +0000

    AD PS Scripts

---
+++ src/powershell/Invoke-GetMonitoringAlerts.ps1
@@ -0,0 +1,36 @@
+﻿cls
+& .\Invoke-SCOMInitialize.ps1
+
+
+ #this call will invoke the command to load the DLL's into memory in order for us to use them in this powershell session
+ Invoke-SCOMInitialize "Z:\projects\scom_updater\lib"
+
+$ConnectorID = "{BCF258E0-2514-4EA5-ABF7-98A8CB208F3A}"
+#$alertId = "BCF258E0-2514-4EA5-ABF7-98A8CB208F3A";
+$alertId = $env:SNC_alertId;
+$scomServerIP = "10.1.10.1";
+#$scomServerIp = $enc:SNC_scomServerIp;
+$scomServerUsername = "tester";
+#$scomServerUsername = $enc:SNC_scomServerUsername;
+$scomServerPassword = "passs";
+#$scomServerPassword = $enc:SNC_scomServerPassword;
+$alertGuid = new-object System.Guid($alertId)
+$comment = "";
+$guid = new-object System.Guid($ConnectorID);
+
+$settings = new-object Microsoft.EnterpriseManagement.ManagementGroupConnectionSettings($scomServerIP);
+$settings.UserName =$scomServerUsername;
+$Password = $scomServerPassword | ConvertTo-SecureString -AsPlainText -Force
+$settings.Password = $Password
+$mg = [Microsoft.EnterpriseManagement.ManagementGroup]::Connect($settings);
+$framework = $mg.ConnectorFramework.GetConnectorFramework();
+$connector = $framework.GetConnector($guid);
+$alert = $mg.OperationalData.GetMonitoringAlert($alertGuid);
+
+#Update the alert
+$alert.Update($comment, $connector);
+
+#Close the alert
+$alert.Refresh();
+$alert.ResolutionState = 0xff;
+$alert.Update($comment, $connector);
---
+++ src/powershell/Invoke-SCOMInitialize.ps1
@@ -0,0 +1,25 @@
+﻿function Invoke-SCOMInitialize {
+param(
+		 $Path
+	)
+	$ErrorActionPreference = 'stop';
+	try{
+		
+		$libs = @("Microsoft.EnterpriseManagement.Core.dll",
+		"Microsoft.EnterpriseManagement.OperationsManager.dll", 
+		"Microsoft.EnterpriseManagement.Runtime.dll" );
+		$scriptpath =  Split-Path -Path $Path
+		$adminui = $(Join-Path -Path $scriptpath -ChildPath 'lib');
+		$libs | %{
+		$(Join-Path -Path $adminui -ChildPath $_)
+			$configmgrprov = get-item -Path $(Join-Path -Path $adminui -ChildPath $_)
+			
+			Add-Type -path $configmgrprov
+		}
+	}catch {
+        $_.Exception.Message
+		return $false;
+	}
+	
+	return $true;
+}
